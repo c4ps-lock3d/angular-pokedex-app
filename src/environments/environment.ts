0e20f28bcaaff2fc9a77c9b417fe90fba62f39b9
Nicolas <nicolas.perrenoud@ik.me>
2026-08-03 16:38:01 +0200

    update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://pokedex.blizzardaudioclub.ch/api/pokemons'
+    apiUrl: 'https://pokedex.blizzardaudioclub.ch/api/randos'
 };
