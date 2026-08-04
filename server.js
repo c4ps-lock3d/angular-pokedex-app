d9e0ee0225a977d24131306729167d3be7cb816e
Nicolas <nicolas.perrenoud@ik.me>
2026-08-04 08:03:41 +0200

    update server.js

--- server.js
+++ server.js
@@ -52,10 +52,10 @@ app.get('/api/randos/:id', async (req, res) => {
       }
     ]).toArray();
     res.json(rando[0]);
-    if (!rando) {
+    //res.json(rando);
+    if (!rando || rando.length === 0) {
       return res.status(404).json({ message: 'Rando non trouvée' });
     }
-    res.json(rando  );
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
